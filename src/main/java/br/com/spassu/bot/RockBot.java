8a95796e3c9faa5df114978705f42d88c3655ef0
Karmaicom Martins <karmaicom@gmail.com>
2019-07-01 00:44:21 -0300

    Criação das Intents iguais as do Dialog Flow.

--- src/main/java/br/com/spassu/bot/RockBot.java
+++ src/main/java/br/com/spassu/bot/RockBot.java
@@ -8,7 +8,6 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
@@ -17,10 +16,10 @@ import javax.net.ssl.HttpsURLConnection;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import com.google.gson.JsonElement;
@@ -62,18 +61,7 @@ public class RockBot extends TelegramLongPollingBot {
 
 		// Command Start
 		if (textUsuario.equals("/start")) {
-			enviarRespostaTelegram("Bem vindo(a) " + update.getMessage().getFrom().getFirstName() + "!", message,
-					update, null, null);
-			mensagemRespondida = true;
-		}
-
-		// Intent welcome
-		List<String> welcome = Arrays.asList("oi", "olá", "hello", "ola", "hi");
-		if (welcome.contains(textUsuario) && mensagemRespondida == false) {
-			List<String> welcomeList = Arrays.asList("Olá, sou o ChatBot do Rock!",
-					"Olá, tudo bem? Bem vindo ao mundo de notícias do Rock!");
-			Random random = new Random();
-			enviarRespostaTelegram(welcomeList.get(random.nextInt(welcomeList.size())), message, update, null, null);
+			enviarRespostaTelegram("Bem vindo(a) " + update.getMessage().getFrom().getFirstName() + "!", message, update, null, null);
 			mensagemRespondida = true;
 		}
 
@@ -84,34 +72,116 @@ public class RockBot extends TelegramLongPollingBot {
 			List<String> agradecerList = Arrays.asList("De nada!", "Disponha.", "Até a próxima.", "Até mais.",
 					"Volte sempre que precisar.", "Foi um prazer ajudar.");
 			Random random = new Random();
-			enviarRespostaTelegram(agradecerList.get(random.nextInt(agradecerList.size())), message, update, null,
-					null);
+			enviarRespostaTelegram(agradecerList.get(random.nextInt(agradecerList.size())), message, update, null, null);
 			mensagemRespondida = true;
 		}
 
-		// Intent bye
-		List<String> bye = Arrays.asList("adeus", "tchau", "bye", "txau");
-		if (bye.contains(textUsuario) && mensagemRespondida == false) {
-			List<String> byeList = Arrays.asList("Nos vemos em breve!", "Obrigado pela conversa!",
-					"Foi bom conversar com você!");
+		// Intent chamar
+		List<String> chamar = Arrays.asList("perguntar", "fazer uma pergunta", "fazer pergunta", "assistente", "e aí", "e ae",
+				"eae", "fala aí", "há", " quanto tempo", "opa", "oi tudo bem", "olá", "fala", "boa noite", "bom dia", "boa tarde", "oiê",
+				"fala ae", "oi");
+		if (chamar.contains(textUsuario) && mensagemRespondida == false) {
+			List<String> chamarList = Arrays.asList("Olá! Sou a Mind! Em quê posso lhe ajudar?",
+					 "Olá! Sou a Mind! Como posso ajudar?",
+					 "Olá! Sou a MIND, a Memória e Inteligência Digital. Como posso te ajudar a aprender hoje?",
+				     "Olá! É um prazer te atender. Sou a Mind! Como posso ajudar?");
 			Random random = new Random();
-			enviarRespostaTelegram(byeList.get(random.nextInt(byeList.size())), message, update, null, null);
+			enviarRespostaTelegram(chamarList.get(random.nextInt(chamarList.size())), message, update, null, null);
 			mensagemRespondida = true;
 		}
 
-		// Intent Segurança
-		List<String> seguranca = Arrays.asList("epi", "segurança");
-		if (seguranca.contains(textUsuario) && mensagemRespondida == false) {
-			List<String> segurancaList = Arrays.asList(
-					"Os equipamentos de segurança devem ser utilizados em locais apropriados.",
-					"Garanta sua segurança utilizando os equipamentos indicados.");
+		// Intent emoji
+		List<String> emoji = Arrays.asList("envia um emoji pra mim", "emoji", "me manda um emoji", "Quero um emoji");
+		if (emoji.contains(textUsuario) && mensagemRespondida == false) {
+			List<String> emojiList = Arrays.asList(":grinning:", ":joy:");
 			Random random = new Random();
-			enviarRespostaTelegram(segurancaList.get(random.nextInt(segurancaList.size())), message, update, null,
-					null);
-			enviarRespostaAutomaticaFoiUtil(message, update);
+			enviarRespostaTelegram(emojiList.get(random.nextInt(emojiList.size())), message, update, null, null);
+			mensagemRespondida = true;
+		}
+		
+		// Intent ofender
+		List<String> ofender = Arrays.asList("sua retardada", "retardada mental", "retardada", "idiota", "sua imbecil", "imbecil", "muito burra",
+				"burra", "vaca", "sua vaca", "deixa de ser estúpida");
+		if (ofender.contains(textUsuario) && mensagemRespondida == false) {
+			List<String> ofenderList = Arrays.asList("Ah, que isso, não precisa ofender. Estou aqui para ajudar.",
+		            "Poxa, que desagradável! Não imaginava passar por isso hoje.",
+		            "Calma, não precisa ficar agressivo(a).  Estou aqui apenas para lhe ajudar.");
+			Random random = new Random();
+			enviarRespostaTelegram(ofenderList.get(random.nextInt(ofenderList.size())), message, update, null, null);
+			mensagemRespondida = true;
+		}
+		
+		// Intent prazo
+		List<String> prazo = Arrays.asList("Quando terei um retorno?", "Essa resposta demora?", "Em quanto tempo terei resposta?", "Quanto tempo vou esperar?",
+				"Isso vai demorar muito?");
+		if (prazo.contains(textUsuario) && mensagemRespondida == false) {
+			List<String> prazoList = Arrays.asList("Só um momento, por favor.",
+		            "Só um instante, estou tetando encontrar a melhor resposta.",
+		            "Só um instante, por favor.",
+		            "Só um instante, estou consultando e já te respondo.",
+		            "Aguarde um instante, por favor. Estou tentando encontrar a melhor resposta.",
+		            "Aguarde um momento, por favor. Estou tentando encontrar e melhor resposta.");
+			Random random = new Random();
+			enviarRespostaTelegram(prazoList.get(random.nextInt(prazoList.size())), message, update, null, null);
 			mensagemRespondida = true;
 		}
 
+		// Intent previsão
+		List<String> previsao = Arrays.asList("previsão do tempo", "previsão chuva", "Qual a previsão do tempo?", "Teremos chuva amanhã?",
+				"Amanhã vai fazer sol?", "Choverá hoje à noite?", "Teremos sol hoje?", "Vai chover hoje?");
+		if (previsao.contains(textUsuario) && mensagemRespondida == false) {
+			List<String> previsaoList = Arrays.asList("Humm... Sobre isso não sei dizer, mas você pode dar uma olhada no Clima Tempo",
+		            "Não sei, não. Já tentou consultar algum canal sobre o tempo?",
+		            "Poxa, não sei informar sobre o tempo, mas você pode dar uma conferida em www.climatempo.com.br");
+			Random random = new Random();
+			enviarRespostaTelegram(previsaoList.get(random.nextInt(previsaoList.size())), message, update, null, null);
+			mensagemRespondida = true;
+		}
+		
+		// Intent não
+		List<String> nao = Arrays.asList("não quero isso", "não faça isso", "acho que não", "não tenho interesse", "definitivamente não", "não obrigado", "não",
+				"discordo", "não mesmo");
+		if (nao.contains(textUsuario) && mensagemRespondida == false) {
+			List<String> naoList = Arrays.asList("Lamento, só tenho informações sobre o EDISE.",
+		            "Que pena, não posso ajudar. No momento só tenho informações sobre o EDISE.",
+		            "Desculpa, mas não tenho como ajudar.  Só me passaram informações sobre o EDISE.");
+			Random random = new Random();
+			enviarRespostaTelegram(naoList.get(random.nextInt(naoList.size())), message, update, null, null);
+			mensagemRespondida = true;
+		}
+		
+		// Intent sim
+		List<String> sim = Arrays.asList("tambem", "também", "correto", "é", "faça isso", "isso mesmo", "sim", "confirmar", "parece bom", "concordo", "exatamente", "claro", "com certeza");
+		if (sim.contains(textUsuario) && mensagemRespondida == false) {
+			List<String> simList = Arrays.asList("Então, o EDISE fica na Avenida Chile, 65",
+		            "Certo, o EDISE fica no Centro, na Avenida Chile, 65",
+		            "O EDISE fica na Avenida Chile, 65. Bem em frente ao prédio do BNDES",
+		            "A sede fica na Avenida Chile, 65, aproximadamente duzentos metros antes da Catedral.");
+			Random random = new Random();
+			enviarRespostaTelegram(simList.get(random.nextInt(simList.size())), message, update, null, null);
+			mensagemRespondida = true;
+		}
+		
+		// Intent perguntar
+		List<String> perguntar = Arrays.asList("Pode me ajudar? Quero saber onde está a Petrobras.", "Sabe onde fica a Petrobras?",
+				"Em que rua fica a Petrobras?", "Onde posso encontrar a Petrobras?");
+		if (perguntar.contains(textUsuario) && mensagemRespondida == false) {
+			List<String> perguntarList = Arrays.asList("Entendi, mas, de qual imóvel Petrobras você gostaria de saber a localidade? É do EDISE?",
+		            "Tudo bem, vou te ajudar com isso. Você está falando do edifício sede da Petrobras?");
+			Random random = new Random();
+			enviarRespostaTelegram(perguntarList.get(random.nextInt(perguntarList.size())), message, update, null, null);
+			mensagemRespondida = true;
+		}
+		
+		// Intent sorrir
+		List<String> sorrir = Arrays.asList(":grinning:");
+		if (sorrir.contains(textUsuario) && mensagemRespondida == false) {
+			List<String> sorrirList = Arrays.asList("Que lindo sorriso!");
+			Random random = new Random();
+			enviarRespostaTelegram(sorrirList.get(random.nextInt(sorrirList.size())), message, update, null, null);
+			mensagemRespondida = true;
+		}
+		
 		if (mensagemRespondida == false) {
 			enviarRespostaAutomatica(message, update);
 			// enviarRespostaTelegram("Ainda não conheço sobre esse assunto. Favor pesquisar
@@ -121,6 +191,7 @@ public class RockBot extends TelegramLongPollingBot {
 
 	}
 
+	/*
 	private void enviarRespostaAutomaticaFoiUtil(SendMessage message, Update update) {
 
 		List<List<InlineKeyboardButton>> buttons = new ArrayList<>();
@@ -138,7 +209,8 @@ public class RockBot extends TelegramLongPollingBot {
 		enviarRespostaTelegram("A resposta foi útil? \nAvalie solecionando uma opção abaixo: ", message, update,
 				markupKeyboard, null);
 	}
-
+	*/
+	
 	private synchronized void enviarRespostaAutomatica(SendMessage message, Update update) {
 
 		/*
@@ -173,14 +245,26 @@ public class RockBot extends TelegramLongPollingBot {
 		// https://zfrdmofcdk.execute-api.us-east-1.amazonaws.com/default/HelloFunction
 		// https://bv29vu8il0.execute-api.sa-east-1.amazonaws.com/DEV/chamador
 		
+		//String username = "rararipe";
+
+		System.out.println("chatId: " + update.getMessage().getChatId());
+		System.out.println("Username: " + update.getMessage().getChat().getUserName());
+		System.out.println("texto: " + update.getMessage().getText());
+		
+		//{'responseId': 'b9de7dd5-d645-44f5-bec7-f84b690a1549-64cfa233','queryResult': {'action': 'acionar'},'originalDetectIntentRequest': {'payload': {'data': {'message': {'from': {'username': 'karmaicom', 'id':null}, 'chat': {'id': null, 'username': 'karmaicom'}, 'text': 'teste'}}}}}
+		//{'responseId': 'b9de7dd5-d645-44f5-bec7-f84b690a1549-64cfa233','queryResult': {'action': 'acionar'},'originalDetectIntentRequest': {'payload': {'data': {'message': {'from': {'username': 'karmaicom', 'id':679539901}, 'chat': {'id': 679539901, 'username': 'karmaicom'}, 'text': 'Teste'}}}}}
+		
 		String jsonInputString =
-				  "{\"responseId\": \"b9de7dd5-d645-44f5-bec7-f84b690a1549-64cfa233\",\r\n" +
-				  " \"queryResult\": {\"action\": \"acionar\"},\r\n" +
-				  " \"originalDetectIntentRequest\": {\"payload\": {\"data\": {\"message\": {\"from\": {\"username\": \"rararipe\", \"id\": 800924953.0\r\n"
-				  +
-				  "                 }, \"chat\": {\"id\": 800924953.0, \"username\": \"rararipe\"\r\n"
-				  + "                 }, \"text\": \"O que é a Mind?\"\r\n" +
-				  "             }\r\n" + "         }\r\n" + "     }\r\n" + " }\r\n" + "}";
+			"{'responseId': 'b9de7dd5-d645-44f5-bec7-f84b690a1549-64cfa233'," +
+					 "'queryResult': {'action': 'acionar'}," +
+					 "'originalDetectIntentRequest': {'payload': {'data': {'message': {'from': {'username': '"+update.getMessage().getChat().getUserName()+"', 'id':" + update.getMessage().getChatId() +
+					                 "}, 'chat': {'id': "+ update.getMessage().getChatId() +", 'username': '" + update.getMessage().getChat().getUserName() +"'" +
+					                 "}, 'text': '"+ update.getMessage().getText() +"'" +
+					             "}" +
+					         "}" +
+					     "}" +
+					 "}" +
+					"}";
 		
 		try {
 			String url = "https://bv29vu8il0.execute-api.sa-east-1.amazonaws.com/DEV/chamador";
@@ -221,9 +305,11 @@ public class RockBot extends TelegramLongPollingBot {
 			}
 			in.close();
 
+			String textMsgRespostaFormatada = textoMsgResposta.toString().replace("\"", "");
+			
 			// print result
 			//System.out.println(response.toString());
-			enviarRespostaTelegram(textoMsgResposta.toString().replace("", null), message, update, null, null);
+			enviarRespostaTelegram(textMsgRespostaFormatada.toString(), message, update, null, null);
 			
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
@@ -235,8 +321,8 @@ public class RockBot extends TelegramLongPollingBot {
 		
 	}
 
-	public void enviarRespostaTelegram(String texto, SendMessage message, Update update,
-			InlineKeyboardMarkup markupKeyboard, ReplyKeyboardMarkup replyKeyboardMarkup) {
+	public void enviarRespostaTelegram(String texto, SendMessage message, Update update, InlineKeyboardMarkup markupKeyboard, ReplyKeyboardMarkup replyKeyboardMarkup) {
+		
 		message.setChatId(update.getMessage().getChatId());
 		message.setText(texto);
 
